131ce3af260109ce0f74b94ef87a987d3ae14161
ryanhjohnston <ryanhjohnstoncollege@gmail.com>
2023-10-11 18:50:56 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+/* (a) (20 points) Write a shared memory OpenMP program on Fox server to multiply two n-by-n matrices using p */
+/* processors with 1<= p <=12. Fill up the matrices with some constant values so that it would be easier for you to */
+/* verify the resulting matrix for correctness. */
+
+int
+main(int argc, char *argv[])
+{
+
+    exit(EXIT_SUCCESS);
+}
